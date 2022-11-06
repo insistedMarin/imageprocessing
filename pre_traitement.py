8218b62f7b7457701ea2a0a156ea79acb843003f
Wenlei <wenlei.luo.ce@outlook.com>
2022-11-06 11:27:12 +0100

    feat: deep learning

--- pre_traitement.py
+++ pre_traitement.py
@@ -9,6 +9,7 @@ from keras import layers
 from keras.models import Model
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from imgaug import augmenters as iaa
 import random
 from keras import optimizers
@@ -132,9 +133,10 @@ def neighbour(L, x, y):
 # Commentaire
 # plt.imshow(x_train[0], cmap='gray')
 plt.title('skeleton')
-# ça arrête le programme ici
-plt.show()
 
+
+# ça arrête le programme ici
+# plt.show()
 
 # plt.figure("Image")
 # plt.imshow(x_train[0], cmap='gray')
@@ -199,7 +201,7 @@ def extration_bifurcation(Matrice):
 # print(crossing_number(new_x_train))
 # plt.imshow(crossing_number(new_x_train))
 plt.title('CN')
-plt.show()
+# plt.show()
 # image = x_real[0].squeeze()
 # preprocess(image)
 # image = x_real[1].squeeze()
@@ -217,12 +219,15 @@ plt.show()
 y_input = np.zeros(20)
 y_input[0:10] = 0
 y_input[10:20] = 1
+print("y_input:")
+print(y_input[0].shape)
 # plt.imshow(x_train[0])
 x_train_2 = x_train[:20:] * (1 / 255)
 
 # 用np定义一个90*90的矩阵，用于存放图片
 
 X_input = np.zeros((20, 90, 90))
+
 
 # print(crossing_number(x_train_2[0]))
 
@@ -236,23 +241,23 @@ def transfer2(data):
 
 x_input = transfer2(x_train_2)
 
-# plt.imshow(x_input[0])
-# plt.imshow(crossing_number(x_input[0]))
-# plt.show()
-# print(x_input.shape)
-# print(x_input[0][51])
-# plt.imshow(x_train_2[0])
+plt.imshow(x_input[0])
+plt.imshow(crossing_number(x_input[0]))
+plt.show()
+print(x_input.shape)
+print(x_input[0][51])
+plt.imshow(x_train_2[0])
 
 (x_train, y_train) = (x_input, y_input)
-
-
-#  ====================
 
 # ====================
 # Build model
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(90, 90)),  # input layer
-    keras.layers.Dense(128, activation='relu'),  # hidden layer
+    keras.layers.Flatten(input_shape=[90, 90]),  # input layer
+    keras.layers.Dense(100, activation='relu'),  # hidden layer
+    keras.layers.Dropout(0.2),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dense(50, activation='relu'),  # hidden layer
     keras.layers.Dropout(0.2),
     keras.layers.BatchNormalization(),
     keras.layers.Dense(2, activation='softmax')  # output layer
@@ -262,26 +267,26 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# optimizers.Adam(lr=0.1)
-
-model.summary()
-
 # Train model
-# print(x_input.shape)
-model.fit(x_input, y_input, epochs=10)
+model.fit(x_train, y_train, epochs=10)
 
 # Evaluate model
-# valid_loss, valid_acc = model.evaluate(x_train, y_train, verbose=1)
-# print(f"Valid loss:{valid_loss}")
-# print(f"Valid accuracy:{valid_acc}")
+valid_loss, valid_acc = model.evaluate(x_train, y_train, verbose=1)
+print(f"Valid loss:{valid_loss}")
+print(f"Valid accuracy:{valid_acc}")
 
-# # Make one prediction
-class_names = ['第一个人', '第二个人']
-y_predicts = model.predict(x_input[0])
-print(y_predicts[0])
-y_index = np.argmax(y_predicts[0])
-print(y_index)
+# Make one prediction
+class_names = ['First Person', 'Second Person']
+y_predicts = model.predict(x_train)
+y_index = np.argmax(y_predicts[2])
 y_label = class_names[y_index]
-print(y_label)
-#
-# # ====================
+print("Number 3 is: ", y_label)
+y_index = np.argmax(y_predicts[9])
+y_label = class_names[y_index]
+print("Number 10 is: ", y_label)
+y_index = np.argmax(y_predicts[10])
+y_label = class_names[y_index]
+print("Number 11 is: ", y_label)
+y_index = np.argmax(y_predicts[19])
+y_label = class_names[y_index]
+print("Number 20 is: ", y_label)
